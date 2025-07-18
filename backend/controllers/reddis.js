2fe7c2bfb48bb054adab29a451882417234e1194
AmanPatre <amanpatre25@gmail.com>
2025-07-18 19:21:00 +0530

    Update

--- backend/controllers/reddis.js
+++ backend/controllers/reddis.js
@@ -3,7 +3,7 @@ import Redis from "ioredis";
 const redisClient = new Redis({
     host :"redis-11355.c212.ap-south-1-1.ec2.redns.redis-cloud.com" ,
     port :"11355",
-    password : "wC4Cj8WeaRlPDmxIqmAEQNVMsw1CFYUi",
+    password :"wC4Cj8WeaRlPDmxIqmAEQNVMsw1CFYUi",
 
 });
 
